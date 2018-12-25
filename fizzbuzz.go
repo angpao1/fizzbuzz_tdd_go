d56a97536cc9436865089e7252ab43628ea3c582
angpao <angpao.kittimasak@gmail.com>
2018-12-25 19:32:03 +0700

    refactor code if to switch case

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -3,14 +3,10 @@ package fizzbuzz_tdd_go
 import "strconv"
 
 func FizzBuzz(input int) string {
-	if input % 15 == 0{
-		return "fizzbuzz"
+	switch {
+	case input % 15 == 0 : return "fizzbuzz"
+	case input % 5 == 0 : return "buzz"
+	case input % 3 == 0 : return "fizz"
+	default : return strconv.Itoa(input)
 	}
-	if input % 5 == 0{
-		return "buzz"
-	}
-	if input % 3 == 0{
-		return "fizz"
-	}
-	return strconv.Itoa(input)
 }
