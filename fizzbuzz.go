bdb3f4711438635b7ad639d653f13cc82aea1678
angpao <angpao.kittimasak@gmail.com>
2018-12-25 19:02:09 +0700

    Test Input 1 return 1 Status = PASSED

---
+++ fizzbuzz.go
@@ -0,0 +1,5 @@
+package fizzbuzz_tdd_go
+
+func FizzBuzz(input int) string {
+	return "1"
+}
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -1,2 +1,11 @@
 package fizzbuzz_tdd_go
 
+import "testing"
+
+func TestInput_1_ShouldBe_1(t *testing.T) {
+	fb := FizzBuzz(1)
+	expected := "1"
+	if fb != expected {
+		t.Errorf("Actual = %s, expected = %s", fb, expected)
+	}
+}
